780c2836f7daa74226f699da3a8ae2dea985cadb
Ginny Mangat <mangatgrinpal@gmail.com>
2020-05-06 23:25:57 -0700

    added laptop preview images to description section

--- app/javascript/components/HeroDescription.js
+++ app/javascript/components/HeroDescription.js
@@ -15,7 +15,7 @@ const HeroDescription = () => {
 					</h3>
 				</Col>
 			</Row>
-			<Row className='justify-content-start text-right hero-description pt-5 mt-5'>
+			<Row className='justify-content-start text-center text-md-right hero-description pt-5 mt-5'>
 				<Col xs={12} md={4}>
 					<h6 className='item-name'>Add your restaurant</h6>
 					<p>
@@ -23,6 +23,11 @@ const HeroDescription = () => {
 						Although this was created with restaurants in mind, 
 						you can track virtually any type of inventory.
 					</p>
+				</Col>
+				<Col className='embed-responsive' xs={12} md={{span: 4, offset:3}}>
+					<img 
+						src='https://ginnysbucket.s3-us-west-1.amazonaws.com/dev-images/add_restaurant_preview_mac.png'
+						className='embed-responsive-item' />
 				</Col>
 			</Row>
 			<Row className='justify-content-start text-right hero-description'>
@@ -33,6 +38,11 @@ const HeroDescription = () => {
 						You'll create your own categories to keep your inventory organized. 
 						You will also specify what you plan to track with each category.
 					</p>
+				</Col>				
+				<Col className='embed-responsive' xs={12} md={{span: 4, offset:3}}>
+					<img 
+						src='https://ginnysbucket.s3-us-west-1.amazonaws.com/dev-images/add_item_view_mac.png'
+						className='embed-responsive-item' />
 				</Col>
 			</Row>
 			<Row className='justify-content-start text-right hero-description'>
